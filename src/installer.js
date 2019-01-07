44ae0b9eedd58666a81e395c9025bbbf8be33755
Mark Lee <electronjs@lazymalevolence.com>
2019-01-06 23:51:28 -0800

    Upgrade fs-extra to ^7

--- src/installer.js
+++ src/installer.js
@@ -258,12 +258,10 @@ var createApplication = function (options, dir, callback) {
   var applicationDir = path.join(dir, 'lib', options.id)
   options.logger('Copying application to ' + applicationDir)
 
-  async.waterfall([
-    async.apply(fs.ensureDir, applicationDir),
-    async.apply(fs.copy, options.src, applicationDir)
-  ], function (err) {
-    callback(err && new Error('Error copying application directory: ' + (err.message || err)))
-  })
+  return fs.ensureDir(applicationDir)
+    .then(() => fs.copy(options.src, applicationDir))
+    .then(() => callback())
+    .catch(err => callback(new Error(`Error copying application directory: ${err.message || err}`)))
 }
 
 /**
