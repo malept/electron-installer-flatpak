bee36e2e4d3cca6da0fd2479a87003f823430c8a
Mark Lee <electronjs@lazymalevolence.com>
2019-03-17 00:10:08 -0700

    Fix script

--- test/ci/script.sh
+++ test/ci/script.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 if [[ "$USE_DOCKER" = "true" ]]; then
-    npm test
-else
     sudo docker run --privileged --interactive --tty --volume $(pwd):/code malept/electron-forge-container:latest /bin/bash -c "cd /code &&
         /code/test/ci/install_runtimes.sh &&
         DEBUG=$DEBUG npm test"
+else
+    npm test
 fi
