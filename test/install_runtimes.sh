5db0a80efb73353697343e6acab962736f8b331c
Mark Lee <electronjs@lazymalevolence.com>
2019-06-11 19:07:21 -0700

    test: refactor Flatpak runtime/app installation
    Use the runtimes/apps from flathub.

--- test/install_runtimes.sh
+++ test/install_runtimes.sh
@@ -1,16 +1,31 @@
 #!/bin/bash -xe
 
+install_app() {
+    local arch="$1"
+    local app="$2"
+    local branch=$3
+    install_flatpak "app/$app/$arch/$branch"
+}
+
 install_runtime() {
     local arch="$1"
     local runtime="$2"
-    local url="$3"
-    if [[ ! -d "$HOME/.local/share/flatpak/runtime/${runtime}.Locale/$arch" ]]; then
-        flatpak install --user --no-deps --assumeyes --arch $arch --from $url
+    local version="$3"
+    install_flatpak "runtime/${runtime}/$arch/$version"
+}
+
+install_flatpak() {
+    local ref="$1"
+    if [[ ! -d "$HOME/.local/share/flatpak/$ref" ]]; then
+        flatpak install --user --no-deps --assumeyes "$ref"
     fi
 }
 
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 for arch in i386 x86_64; do
-    install_runtime $arch org.freedesktop.Sdk https://raw.githubusercontent.com/endlessm/flatpak-bundler/master/refs/freedesktop-sdk-1.4.flatpakref
-    install_runtime $arch org.freedesktop.Platform https://raw.githubusercontent.com/endlessm/flatpak-bundler/master/refs/freedesktop-runtime-1.4.flatpakref
-    install_runtime $arch io.atom.electron.BaseApp https://s3-us-west-2.amazonaws.com/electron-flatpak.endlessm.com/electron-base-app-master.flatpakref
+    install_runtime $arch org.freedesktop.Sdk 1.6
+    install_runtime $arch org.freedesktop.Platform 1.6
+    install_app $arch io.atom.electron.BaseApp stable
+    install_app $arch org.electronjs.Electron2.BaseApp stable
 done
