4b1622f7fa3254e6f0bef2a417a7308773021b21
Mark Lee <malept@users.noreply.github.com>
2020-06-06 09:22:28 -0700

    fix: provide defaults for the *Flatpakref options (#26)
    * refactor: move --assumeyes support to flatpak-bundler & update options

--- src/installer.js
+++ src/installer.js
@@ -1,16 +1,12 @@
 'use strict'
 
 const _ = require('lodash')
-const childProcess = require('child_process')
 const common = require('electron-installer-common')
 const debug = require('debug')
 const flatpak = require('@malept/flatpak-bundler')
 const { getAppID } = require('./getappid')
 const path = require('path')
-const { promisify } = require('util')
 const semver = require('semver')
-
-const exec = promisify(childProcess.exec)
 
 const defaultLogger = debug('electron-installer-flatpak')
 const defaultRename = (dest, src) => path.join(dest, src)
@@ -36,6 +32,15 @@ class FlatpakInstaller extends common.ElectronInstaller {
 
   get defaultDesktopTemplatePath () {
     return path.join(this.resourcesDir, 'desktop.ejs')
+  }
+
+  get flatpakrefs () {
+    const arch = flatpak.translateArch(this.options.arch)
+    return {
+      baseFlatpakref: this.options.baseFlatpakref || `app/${this.options.base}/${arch}/${this.options.baseVersion}`,
+      runtimeFlatpakref: this.options.runtimeFlatpakref || `runtime/${this.options.runtime}/${arch}/${this.options.runtimeVersion}`,
+      sdkFlatpakref: this.options.sdkFlatpakref || `runtime/${this.options.sdk}/${arch}/${this.options.runtimeVersion}`
+    }
   }
 
   get resourcesDir () {
@@ -144,12 +149,6 @@ class FlatpakInstaller extends common.ElectronInstaller {
       extraExports.push(this.pixmapIconPath)
     }
 
-    const flatpakBundlerVersion = (await exec('flatpak-builder --version')).toString().split(' ', 2)[1]
-
-    if (flatpakBundlerVersion.split('.').map(part => Number(part)) >= [0, 9, 9]) {
-      this.options.extraFlatpakBuilderArgs.push('--assumeyes')
-    }
-
     const files = [
       [this.stagingDir, '/']
     ]
@@ -164,22 +163,20 @@ class FlatpakInstaller extends common.ElectronInstaller {
       branch: this.options.branch,
       base: this.options.base,
       baseVersion: this.options.baseVersion.toString(),
-      baseFlatpakref: this.options.baseFlatpakref,
-      extraFlatpakBuilderArgs: this.options.extraFlatpakBuilderArgs,
       runtime: this.options.runtime,
       runtimeVersion: this.options.runtimeVersion.toString(),
-      runtimeFlatpakref: this.options.runtimeFlatpakref,
       sdk: this.options.sdk,
-      sdkFlatpakref: this.options.sdkFlatpakref,
       finishArgs: this.options.finishArgs,
       command,
-      files: files.concat(this.options.files),
-      symlinks: symlinks.concat(this.options.symlinks),
-      extraExports: extraExports,
       modules: this.options.modules
     }, {
+      ...this.flatpakrefs,
       arch: this.options.arch,
-      bundlePath: dest
+      bundlePath: dest,
+      extraExports: extraExports,
+      extraFlatpakBuilderArgs: this.options.extraFlatpakBuilderArgs,
+      files: files.concat(this.options.files),
+      symlinks: symlinks.concat(this.options.symlinks)
     })
   }
 }
