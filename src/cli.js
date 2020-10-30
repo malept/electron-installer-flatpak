b5a55166bfa4a9d6ac6d7d1429da27197a657e62
Mark Lee <electronjs@lazymalevolence.com>
2020-10-29 18:52:23 -0700

    refactor: fix lint warnings & move CLI script to bin/

--- src/cli.js
+++ src/cli.js
@@ -1,43 +1,44 @@
-#!/usr/bin/env node
+'use strict'
 
-var _ = require('lodash')
-var yargs = require('yargs')
+const { omit } = require('lodash')
+const yargs = require('yargs')
 
-var installer = require('./installer')
-var pkg = require('../package.json')
+const installer = require('./installer')
+const { description, version } = require('../package.json')
 
-var argv = yargs
-  .version(pkg.version)
-  .usage(pkg.description + '\n\nUsage: $0 --src <inputdir> --dest <outputdir> --arch <architecture>')
-  .option('src', {
-    describe: 'Directory that contains your built Electron app (e.g. with `electron-packager`)',
-    demand: true
-  })
-  .option('dest', {
-    describe: 'Directory that will contain the resulting Flatpak',
-    demand: true
-  })
-  .option('arch', {
-    describe: 'Machine architecture the package is targeted to',
-    demand: true
-  })
-  .option('config', {
-    describe: 'JSON file that contains the metadata for your application',
-    config: true
-  })
-  .example('$0 --src dist/app/ --dest dist/installer/ --arch ia32', 'use metadata from `dist/app/`')
-  .example('$0 --src dist/app/ --dest dist/installer/ --config config.json', 'use metadata from `config.json`')
-  .wrap(null)
-  .argv
+module.exports = {
+  parseArgs: function parseArgs (args) {
+    return yargs
+      .version(version)
+      .usage(`${description}\n\nUsage: $0 --src <inputdir> --dest <outputdir> --arch <architecture> [...]`)
+      .option('src', {
+        describe: 'Directory that contains your built Electron app (e.g. with `electron-packager`)',
+        demand: true
+      })
+      .option('dest', {
+        describe: 'Directory that will contain the resulting Flatpak',
+        demand: true
+      })
+      .option('arch', {
+        describe: 'Target machine architecture for the package',
+        demand: true
+      })
+      .option('config', {
+        describe: 'JSON file that contains the metadata for your application',
+        config: true
+      })
+      .example('$0 --src dist/app/ --dest dist/installer/ --arch ia32', 'use metadata from `dist/app/`')
+      .example('$0 --src dist/app/ --dest dist/installer/ --config config.json', 'use metadata from `config.json`')
+      .wrap(null)
+      .parse(args)
+  },
+  run: async function run (args) {
+    const argv = module.exports.parseArgs()
 
-console.log('Creating package (this may take a while)')
+    console.log('Creating package (this may take a while)')
 
-var options = _.omit(argv, ['$0', '_', 'version'])
-installer(options, function (err) {
-  if (err) {
-    console.error(err, err.stack)
-    process.exit(1)
+    const options = omit(argv, ['$0', '_', 'version'])
+    await installer(options)
+    console.log(`Successfully created package at ${argv.dest}`)
   }
-
-  console.log('Successfully created package at ' + argv.dest)
-})
+}
